25df18911942e7cf52b981fefac16b2423ba37e8
yanqi <1321490064@qq.com>
2025-04-18 20:26:26 +0800

    Token解析

--- online-taxi-public/internal-common/src/main/java/com/msb/internalcommon/util/JwtUtils.java
+++ online-taxi-public/internal-common/src/main/java/com/msb/internalcommon/util/JwtUtils.java
@@ -3,6 +3,8 @@ package com.msb.internalcommon.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.junit.Test;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -12,9 +14,16 @@ import java.util.Map;
 public class JwtUtils {
     //盐
     private static final String SIGN = "JFSSJ@$$@%";
+    private static final String JWT_KEY = "passengerPhone";
 
-    //生成token
-    public static String generatorToken(Map<String, String> map){
+    /**
+     * 生成token
+     * @param passengerPhone
+     * @return
+     */
+    public static String generatorToken(String passengerPhone){
+        Map<String, String> map = new HashMap<>();
+        map.put(JWT_KEY,passengerPhone);
         //创建builder
         JWTCreator.Builder builder = JWT.create();
         //设置token过期时间并整合过期时间
@@ -22,7 +31,6 @@ public class JwtUtils {
         calendar.add(Calendar.DATE, 1);
         Date time = calendar.getTime();
         builder.withExpiresAt(time);
-
         //整合map
         map.forEach((k, v) -> {builder.withClaim(k,v);});
         //生成token
@@ -30,12 +38,21 @@ public class JwtUtils {
         return sign;
     }
 
-    public static void main(String[] args) {
-        Map<String, String> objectObjectHashMap = new HashMap<>();
-        objectObjectHashMap.put("id", "1");
-        objectObjectHashMap.put("name","张三");
-        String s = generatorToken(objectObjectHashMap);
-        System.out.println(s);
+    /**
+     * 解析Token
+     * @param passengerPhone
+     * @return
+     */
+    public static String parseToken(String passengerPhone){
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(passengerPhone);
+        return verify.getClaim(JWT_KEY).toString();
+    }
 
+    @Test
+    public void test() {
+        String s = generatorToken("123456789");
+        System.out.println(s);
+        String s1 = parseToken(s);
+        System.out.println(s1);
     }
 }
