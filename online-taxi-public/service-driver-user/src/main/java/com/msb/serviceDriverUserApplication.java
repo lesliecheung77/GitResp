c34248cb9cb231c4af1122460dca8b73c9116e6f
yanqi <1321490064@qq.com>
2025-04-23 00:18:18 +0800

    service-driver框架

---
+++ online-taxi-public/service-driver-user/src/main/java/com/msb/serviceDriverUserApplication.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class serviceDriverUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(serviceDriverUserApplication.class, args);
+    }
+}
