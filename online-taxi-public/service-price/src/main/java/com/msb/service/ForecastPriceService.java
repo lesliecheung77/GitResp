3c02ef0fcfe4835f42129bfd3c5d5b2c9c526a51
yanqi <1321490064@qq.com>
2025-04-22 11:07:45 +0800

    返回预估价格

--- online-taxi-public/service-price/src/main/java/com/msb/service/ForecastPriceService.java
+++ online-taxi-public/service-price/src/main/java/com/msb/service/ForecastPriceService.java
@@ -9,9 +9,12 @@ import com.msb.internalcommon.responese.ForecastPriceResponse;
 import com.msb.mapper.PriceRuleMapper;
 import com.msb.remote.MapServiceClient;
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.cms.bc.BcKEKRecipientInfoGenerator;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 
@@ -38,8 +41,10 @@ public class ForecastPriceService {
         forecastPriceDto.setDestLongitude(destLongitude);
         forecastPriceDto.setDestLatitude(destLatitude);
         ResponseResult<DirectionResponse> direction = mapServiceClient.mapService(forecastPriceDto);
-        log.info("距离：" + direction.getData().getDistance() + "米");
-        log.info("时间：" + direction.getData().getDistance() / 60 + "分钟");
+        Integer distance = direction.getData().getDistance();
+        Integer duration = direction.getData().getDistance();
+        log.info("距离：" + distance + "米");
+        log.info("时间：" + duration / 60 + "分钟");
 
         //读取计价规则
         HashMap<String, Object> objectHashMap = new HashMap<>();
@@ -52,10 +57,58 @@ public class ForecastPriceService {
         PriceRule priceRule = priceRules.get(0);
         log.info(priceRule.getCityCode());
         log.info(priceRule.getVehicleType());
-        //根据距离、时间计算出价格
+        //根据距离、时间和计价规则计算出预估价格
+        double price = getPrice(distance, duration, priceRule);
+
         ForecastPriceResponse forecastPrice = new ForecastPriceResponse();
-        forecastPrice.setPrice(12);
+        forecastPrice.setPrice(price);
         return ResponseResult.success(forecastPrice);
     }
 
+    /**
+     *根据这三个参数计算预估价格
+     * @param distance  实际距离
+     * @param duration  实际时长
+     * @param priceRule 计价规则
+     * @return
+     */
+    private static double getPrice(Integer distance, Integer duration,PriceRule priceRule){
+        //初始化预估价格
+        BigDecimal price = new BigDecimal(10);
+
+        //获取实际公里数和起步公里数，并计算出收费里程数
+        BigDecimal distanceMeterBigdecimal = new BigDecimal(distance);
+        BigDecimal distanceBigdecimal = distanceMeterBigdecimal.divide(BigDecimal.valueOf(1000), 2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal startMileBigDecimal = new BigDecimal(priceRule.getStartMile());
+        Double moreMile = distanceBigdecimal.subtract(startMileBigDecimal).doubleValue();
+        Double actualMile = moreMile < 0 ? 0 : moreMile;  //如果实际里程没有超过起步价不算入里程数
+        BigDecimal actualMileDecimal = new BigDecimal(actualMile);
+
+        //获取收费里程数每公里价格,计算超出起始里程的价格
+        BigDecimal PricePerMile = new BigDecimal(priceRule.getUnitPricePerMile());
+        BigDecimal mileFare = actualMileDecimal.multiply(PricePerMile).setScale(2, BigDecimal.ROUND_HALF_UP);
+        price = price.add(mileFare);
+
+        //获取总时长
+        BigDecimal bigDecimal = new BigDecimal(duration);
+        BigDecimal durationBigDecimal = bigDecimal.divide(new BigDecimal(60), 2, BigDecimal.ROUND_HALF_UP);
+        //获取时长单价
+        Double unitPricePerMinuteDouble = priceRule.getUnitPricePerMinute();
+        BigDecimal PricePerMinute = new BigDecimal(unitPricePerMinuteDouble);
+        //计算总时长价格
+        BigDecimal durationPrice = durationBigDecimal.multiply(PricePerMinute);
+        price = price.add(durationPrice).setScale(2, BigDecimal.ROUND_HALF_UP);
+
+        return price.doubleValue();
+    }
+    @Test
+    public void forecastPriceTest(){
+        PriceRule priceRule = new PriceRule();
+        priceRule.setStartMile(3.0);
+        priceRule.setStartFare(10.0);
+        priceRule.setUnitPricePerMile(1.8);
+        priceRule.setUnitPricePerMinute(0.5);
+        double price = getPrice(6500, 1800, priceRule);
+        System.out.println(price);
+    }
 }
