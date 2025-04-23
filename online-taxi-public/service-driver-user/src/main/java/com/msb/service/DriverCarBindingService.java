9eeac670c22011566da97ec610debb9a3a23eadb
yanqi <1321490064@qq.com>
2025-04-24 01:07:15 +0800

    1

--- online-taxi-public/service-driver-user/src/main/java/com/msb/service/DriverCarBindingService.java
+++ online-taxi-public/service-driver-user/src/main/java/com/msb/service/DriverCarBindingService.java
@@ -1,5 +1,8 @@
 package com.msb.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.msb.internalcommon.constant.CommonStatusEnum;
+import com.msb.internalcommon.constant.DriverCarConstants;
 import com.msb.internalcommon.dto.DriverCarBindingRelationship;
 import com.msb.internalcommon.dto.ResponseResult;
 import com.msb.mapper.DriverCarBindingMapper;
@@ -14,8 +17,21 @@ public class DriverCarBindingService {
     private DriverCarBindingMapper driverCarBindingMapper;
 
     public ResponseResult bindDriverCar(DriverCarBindingRelationship driverCarBindingRelationship){
+        //如果司机和车辆已经做过绑定，则不再进行绑定
+        QueryWrapper<DriverCarBindingRelationship> queryWrapper = new QueryWrapper<>();
+        //添加查询条件
+        queryWrapper.eq("driverId", driverCarBindingRelationship.getDriverId());
+        queryWrapper.eq("carId", driverCarBindingRelationship. getCarId());
+        queryWrapper.eq("bindState",DriverCarConstants.DRIVER_CAR_BIND);
+        Integer i = driverCarBindingMapper.selectCount(queryWrapper);
+        if(i.intValue() > 0){
+            return ResponseResult.fail(CommonStatusEnum.DRIVER_CAR_BIND_EXISTS.getCode(),CommonStatusEnum.DRIVER_CAR_BIND_EXISTS.getValue());
+        }
+
+
         LocalDateTime now = LocalDateTime.now();
         driverCarBindingRelationship.setBindingTime(now);
+        driverCarBindingRelationship.setBindState(DriverCarConstants.DRIVER_CAR_BIND);
         driverCarBindingMapper.insert(driverCarBindingRelationship);
         return ResponseResult.success("insert success");
     }
